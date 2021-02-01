d5aaacff53f2481c928a8344726a90d3bb4ca79c
Gregory <gregory.mkv@gmail.com>
2021-02-01 00:05:54 -0500

    Remove dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as rootStore } from "./root";
-export { default as store } from "./store";
--- dist/root.d.ts
+++
@@ -1,7 +0,0 @@
-import { Immutable, Store } from "./common";
-declare const rootStore: <T extends Record<string, Store<any>>>(map: T) => {
-    enableDebugging: () => void;
-    getState: () => Immutable<T>;
-    useStore<MP>(f: (props: T) => MP): Immutable<MP>;
-};
-export default rootStore;
