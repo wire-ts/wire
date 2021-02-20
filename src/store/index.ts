953958a7e26920e03f77c4b968c34b9f1db3866c
Gregory <gregory.mkv@gmail.com>
2021-02-20 13:45:58 -0500

    Store - add selectors

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,13 +6,17 @@ const createStore = <S>(initialState: S): Input.StoreWithState<S> => ({
   actions: <A extends Input.Actions<S>>(actions: A) => {
     const subscribers = new Subscribers();
 
-    const store: Output.BasicStore<S, A> = {
+    const store: Output.Store<S, A, null, null> = {
       state: initialState as Immutable<S>,
       subscribe: subscribers.add,
       actions: {} as Output.Actions<S, A>,
       thunks: <T extends Input.Thunks>(thunks: T) => {
         (store as any).thunks = thunks;
-        return (store as any) as Output.StoreWithThunks<S, A, T>;
+        return store as Output.Store<S, A, Output.Thunks<T>, null>;
+      },
+      selectors: <I extends Input.Selectors<S>>(selectors: I) => {
+        (store as any).selectors = selectors;
+        return store as Output.Store<S, A, null, Output.Selectors<S, I>>;
       },
     };
 
@@ -22,8 +26,8 @@ const createStore = <S>(initialState: S): Input.StoreWithState<S> => ({
   },
 });
 
-const transformActions = <S, A extends Input.Actions<S>>(
-  store: Output.BasicStore<S, A>,
+const transformActions = <S, A extends Input.Actions<S>, St extends Output.Store<S, A, any, any>>(
+  store: St,
   broadcast: (method: string) => void,
   actions: A
 ) => {
