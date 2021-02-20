46829a77a9338e3b945a67326aca2bc9909ce126
Gregory <gregory.mkv@gmail.com>
2021-02-20 13:45:35 -0500

    Add selectors

--- src/store/types.ts
+++ src/store/types.ts
@@ -3,7 +3,7 @@ import { SubscribeFn, Immutable } from "../common";
 export namespace Input {
   export type Actions<S> = Record<
     string,
-    | ((state: Immutable<S>, ...args: never[]) => Immutable<S>)
+    (state: Immutable<S>, ...args: never[]) => Immutable<S>
   >;
 
   export type Thunks = Record<
@@ -12,8 +12,15 @@ export namespace Input {
   >;
 
   export type StoreWithState<S> = {
-    actions: <A extends Actions<S>>(actions: A) => Output.BasicStore<S, A>;
+    actions: <A extends Actions<S>>(
+      actions: A
+    ) => Output.Store<S, A, null, null>;
   };
+
+  export type Selectors<S> = Record<
+    string,
+    (state: Immutable<S>, ...args: never[]) => any
+  >;
 }
 
 export namespace Output {
@@ -26,20 +33,30 @@ export namespace Output {
       : never;
   };
 
+  export type Selectors<S, I extends Input.Selectors<S>> = {
+    [k in keyof I]: I[k] extends (
+      state: Immutable<S>,
+      ...args: infer Args
+    ) => infer O
+      ? (...args: Args) => O
+      : never;
+  };
+
   export type Thunks<T extends Input.Thunks> = T;
 
-  export type BasicStore<S, A extends Input.Actions<S>> = {
+  export type Store<S, A extends Input.Actions<S>, T, Sel> = {
     state: Immutable<S>;
     subscribe: (f: SubscribeFn) => () => void;
     actions: Actions<S, A>;
-    thunks: <T extends Input.Thunks>(thunks: T) => StoreWithThunks<S, A, T>;
-  };
-
-  export type StoreWithThunks<
-    S,
-    A extends Input.Actions<S>,
-    T extends Input.Thunks
-  > = Omit<BasicStore<S, A>, "thunks"> & {
-    thunks: Thunks<T>;
+    thunks: T extends null
+      ? <I extends Input.Thunks>(
+          thunks: I
+        ) => Store<S, A, Output.Thunks<I>, Sel>
+      : T;
+    selectors: Sel extends null
+      ? <I extends Input.Selectors<S>>(
+          selectors: I
+        ) => Store<S, A, T, Output.Selectors<S, I>>
+      : Sel;
   };
 }
